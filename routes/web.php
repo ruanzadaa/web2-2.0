139db1007321f8fc77e5b803a769de464af302cf
ruanzadaa <rlbl@discente.ifpe.edu.br>
2024-09-21 23:59:21 -0300

    adicionado a pratica-6 criacao das views de books, authors, categories e publishers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PublisherController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Rotas para Books
+Route::resource('books', BookController::class);
+
+// Rotas para Authors
+Route::resource('authors', AuthorController::class);
+
+// Rotas para Categories
+Route::resource('categories', CategoryController::class);
+
+// Rotas para Publishers
+Route::resource('publishers', PublisherController::class);
